60ca669744e5e9f266e6f9f3997d873076e66961
也许丶 <myyearn545@gmail.com>
2020-11-11 15:01:40 +0800

    登录接口

---
+++ controller/loginController.go
@@ -0,0 +1,18 @@
+// @Author : Lik
+// @Time   : 2020/11/11
+package controller
+
+import "github.com/gin-gonic/gin"
+
+type LoginController struct {
+}
+
+func (*LoginController) login(rg *gin.RouterGroup) {
+	rg.GET("/login", func(c *gin.Context) {
+		name, _ := c.Params.Get("name")
+		c.JSON(200, gin.H{
+			"name": name,
+			"age":  12,
+		})
+	})
+}
